21c322a948c55b0b0665a87f20703103b1a42e9b
Coolll <2502127576@qq.com>
2017-06-21 16:30:15 +0800

    Add files via upload
    测试Push时CPU状态

---
+++ WQLPaoMaView/SecondViewController.m
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.m
+//  TestPaoma
+//
+//  Created by 龙培 on 17/6/21.
+//  Copyright © 2017年 龙培. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor lightGrayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
