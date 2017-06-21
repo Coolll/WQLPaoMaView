ee0fe24716397bb71ce107006d7f6f66c6d7561c
Coolll <2502127576@qq.com>
2017-06-21 16:32:29 +0800

    Update AppDelegate.m

--- WQLPaoMaView/AppDelegate.m
+++ WQLPaoMaView/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,9 +17,22 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    ViewController *vc = [[ViewController alloc]init];
+    
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    
+    [self.window makeKeyAndVisible];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    UINavigationController *navi = [[UINavigationController alloc]initWithRootViewController:vc];
+    
+    self.window.rootViewController = navi;
+    
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
